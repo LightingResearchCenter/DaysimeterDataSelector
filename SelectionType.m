49256d9e760f640cae0d6b9e1d81344694453132
Geoffrey Jones <jonesg5@rpi.edu>
2017-03-15 14:37:57 -0400

    Created SelectionType classdef

---
+++ SelectionType.m
@@ -0,0 +1,9 @@
+classdef SelectionType
+    %SELECTIONTYPE Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    enumeration
+        Bed,Error,Noncompliance,Observation
+    end
+end
+
